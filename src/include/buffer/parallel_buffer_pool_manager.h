91ba779676235727859d22f138db126f050635cc
qian0817 <qianlei666@gmail.com>
2022-06-05 23:40:44 +0800

    project1 Task #3 - Parallel Buffer Pool Manager

--- src/include/buffer/parallel_buffer_pool_manager.h
+++ src/include/buffer/parallel_buffer_pool_manager.h
@@ -13,6 +13,7 @@
 #pragma once
 
 #include "buffer/buffer_pool_manager.h"
+#include "buffer/buffer_pool_manager_instance.h"
 #include "recovery/log_manager.h"
 #include "storage/disk/disk_manager.h"
 #include "storage/page/page.h"
@@ -86,5 +87,10 @@ class ParallelBufferPoolManager : public BufferPoolManager {
    * Flushes all the pages in the buffer pool to disk.
    */
   void FlushAllPgsImp() override;
+
+ private:
+  size_t num_instances_;
+  size_t pool_size_;
+  std::vector<BufferPoolManagerInstance *> buffer_pools_;
 };
 }  // namespace bustub
